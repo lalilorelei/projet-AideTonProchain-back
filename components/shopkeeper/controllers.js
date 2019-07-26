1951eb6dcf315c3e4bb1d648ee02456641e042e2
ibrahim <duranteau.l.i@gmail.com>
2019-07-26 19:37:10 +0100

    add (shopkeeper) : routes -controllers
    Add routes and controllers for products

--- components/shopkeeper/controllers.js
+++ components/shopkeeper/controllers.js
@@ -1,5 +1,7 @@
 // const bcrypt = require('bcrypt');
 // const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
 const Shopkeeper = require('./model');
 const utilCtlr = require('../utils/controllers/index');
 
@@ -10,3 +12,85 @@ module.exports.connexion = utilCtlr.connexion(Shopkeeper);
 module.exports.profil = utilCtlr.profil(Shopkeeper);
 
 module.exports.profil_update = utilCtlr.profil_update(Shopkeeper);
+
+module.exports.product_creation = async (req, res) => {
+  try {
+    const { shopkeeperId } = req.params;
+    const shopkeeper = await Shopkeeper.findById({ _id: shopkeeperId });
+    if (!shopkeeper) {
+      res.status(404).send({ error: 'Invalid shopkeeper' });
+    }
+
+    await Shopkeeper.findOneAndUpdate(
+      { _id: shopkeeperId },
+      { $push: { products: { _id: mongoose.Types.ObjectId(), ...req.body } } },
+    );
+
+    const product = await Shopkeeper.aggregate([
+      { $project: { last: { $arrayElemAt: ['$products', -1] } } },
+    ]);
+
+    res.status(201).send(product);
+  } catch (e) {
+    res.status(400).send();
+  }
+};
+
+module.exports.products = async (req, res) => {
+  try {
+    const { shopkeeperId } = req.params;
+    const shopkeeper = await Shopkeeper.findById({ _id: shopkeeperId });
+    if (!shopkeeper) {
+      res.status(404).send();
+    }
+
+    res.send(shopkeeper.products);
+  } catch (e) {
+    res.status(400).send();
+  }
+};
+
+module.exports.product = async (req, res) => {
+  try {
+    const { shopkeeperId, id } = req.params;
+    const shopkeeper = await Shopkeeper.findById({ _id: shopkeeperId });
+    if (!shopkeeper) {
+      res.status(404).send({ error: 'Invalid shopkeeper' });
+    }
+
+    const index = shopkeeper.products.findIndex(el => String(el._id) === id);
+    // const product = await Shopkeeper.findById({ _id: shopkeeperId, products: id });
+
+    if (index === -1) {
+      res.status(404).send({ error: 'Invalid product' });
+    }
+
+    res.send(shopkeeper.products[index]);
+  } catch (e) {
+    res.status(400).send();
+  }
+};
+
+module.exports.product_update = async (req, res) => {
+  const { shopkeeperId, id } = req.params;
+  const updates = Object.keys(req.body);
+  try {
+    const shopkeeper = await Shopkeeper.findById({ _id: shopkeeperId });
+    if (!shopkeeper) {
+      res.status(404).send({ error: 'Invalid shopkeeper' });
+    }
+
+    const index = shopkeeper.products.findIndex(el => String(el._id) === id);
+
+    if (index === -1) {
+      res.status(404).send({ error: 'Invalid product' });
+    }
+
+    updates.forEach(update => (shopkeeper.products[index][update] = req.body[update]));
+    await shopkeeper.save();
+
+    res.send(shopkeeper.products[index]);
+  } catch (e) {
+    res.status(400).send();
+  }
+};
--- components/shopkeeper/routes.js
+++ components/shopkeeper/routes.js
@@ -9,4 +9,9 @@ router.post('/connexion', shopkeeperCtlr.connexion);
 router.get('/profil/:id', shopkeeperCtlr.profil);
 router.patch('/profil-update/:id', shopkeeperCtlr.profil_update);
 
+router.post('/product/:shopkeeperId/', shopkeeperCtlr.product_creation);
+router.get('/products/:shopkeeperId', shopkeeperCtlr.products);
+router.get('/product/:shopkeeperId/:id', shopkeeperCtlr.product);
+router.patch('/product-update/:shopkeeperId/:id', shopkeeperCtlr.product_update);
+
 module.exports = router;
